b61192f0ba467d4b1d3b8ad8bc405c5da8bb65fa
weisite <site.wei@thoughtworks.com>
2022-03-23 17:16:51 +0800

    feat: added list in DiscoverView

--- UI-Component/Views/DiscoverView.swift
+++ UI-Component/Views/DiscoverView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct DiscoverView: View {
+    init() {
+        UITableView.appearance().backgroundColor = .clear
+    }
     var body: some View {
-        Text("发现页面")
+        VStack {
+            Text("发现")
+            List {
+                HStack() {
+                    Image(systemName: "circle.circle.fill")
+                        .frame(width: 20, height: 20)
+                    Text("朋友圈")
+                        .offset(x: 6)
+                }
+                HStack() {
+                    Image(systemName: "gamecontroller.fill")
+                        .frame(width: 20, height: 20)
+                    Text("游戏")
+                        .offset(x: 6)
+                }
+                HStack() {
+                    Image(systemName: "circle.dashed")
+                        .frame(width: 20, height: 20)
+                    Text("小程序")
+                        .offset(x: 6)
+                }
+            }
+        }
+        .background(Color(.systemGray6))
     }
 }
 
