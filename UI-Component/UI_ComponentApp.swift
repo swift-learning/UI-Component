3f60dc8e5f49c0f9ac1f83475a4ee0faa932febd
weisite <site.wei@thoughtworks.com>
2022-03-23 14:45:18 +0800

    Initial Commit

---
+++ UI-Component/UI_ComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_ComponentApp.swift
+//  UI-Component
+//
+//  Created by weisite on 2022/3/23.
+//
+
+import SwiftUI
+
+@main
+struct UI_ComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
