696b7afe2ba022bee1ff3a099b3b45028c0c6dd0
weisite <site.wei@thoughtworks.com>
2022-03-24 14:04:41 +0800

    feat: support 1 picture in moment

---
+++ UI-Component/Views/MomentsItems/MomentPicsView.swift
@@ -0,0 +1,20 @@
+//
+//  MomentPicsView.swift
+//  UI-Component
+//
+//  Created by weisite on 2022/3/24.
+//
+
+import SwiftUI
+
+struct MomentPicsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MomentPicsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MomentPicsView()
+    }
+}
