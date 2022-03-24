d9955b7b359285bd4d224c452c0a97f903583a0d
weisite <site.wei@thoughtworks.com>
2022-03-24 13:46:08 +0800

    feat: added MomentView for content only moment

---
+++ UI-Component/Views/MomentsItems/MomentView.swift
@@ -0,0 +1,20 @@
+//
+//  MomentView.swift
+//  UI-Component
+//
+//  Created by weisite on 2022/3/24.
+//
+
+import SwiftUI
+
+struct MomentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MomentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MomentView()
+    }
+}
