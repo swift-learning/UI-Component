006d1b429d13865849f22e22366ee5eb2d802025
weisite <site.wei@thoughtworks.com>
2022-03-25 16:12:17 +0800

    facilitated MyInfoView

--- UI-Component/Views/MyInfoView.swift
+++ UI-Component/Views/MyInfoView.swift
@@ -8,7 +8,33 @@
 import SwiftUI
 
 struct MyInfoView: View {
+    @EnvironmentObject var user: User
+    
     var body: some View {
-        Text("我页面")
+        VStack {
+            NavigationView {
+                List {
+                    NavigationLink(destination: Image(user.avatar)) {
+                        HStack(alignment: .top) {
+                            Image(user.avatar)
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                            Text(user.name)
+                                .fontWeight(.bold)
+                        }
+                    }
+                }
+                .navigationTitle("我")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .background(Color(.systemGray6))
+        }
+    }
+}
+
+struct MyInfoView_Preview: PreviewProvider {
+    static var previews: some View {
+        MyInfoView()
+            .environmentObject(defaultUser)
     }
 }
